2b61d7b506308d7e99c7fa35e2af329c75421e38
NguyenPhuongDuy99 <phuongduy@evol.vn>
2025-03-14 11:24:23 +0700

    feat: create table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,27 @@
 
 import StackedBarChart from "@/components/BarChart";
 import HighPieChart from "@/components/HighPieChart";
+import TableChart from "@/components/TableChart";
 import { Lock, LockOpen } from "lucide-react";
 
 export default function Home() {
+  const data = [
+    { name: "Strategic Partner", unlocked: 25, locked: 15, color: "#E210E3" },
+    {
+      name: "Core team",
+      unlocked: 2.5,
+      locked: 17.5,
+      color: "#4AC8FF",
+    },
+    { name: "Founding Community", unlocked: 4, locked: 11, color: "#A68DFF" },
+    { name: "Airdrop Campaigns", unlocked: 15, locked: 0, color: "#7543FF" },
+    {
+      name: "Community and Ecosystem",
+      unlocked: 10,
+      locked: 0,
+      color: "#1C2ED3",
+    },
+  ];
   return (
     <div className="px-2.5 text-white h-screen bg-[url(/img/background-img.webp)] bg-no-bg-no-repeat bg-cover bg-center">
       <div className="pt-[52px] max-w-[1200px] w-full mx-auto flex flex-col gap-5">
@@ -24,7 +42,10 @@ export default function Home() {
           <StackedBarChart />
           <Lock className="shrink-0" />
         </div>
-        <HighPieChart />
+        <div className="flex justify-between">
+          <TableChart data={data} />
+          <HighPieChart data={data} />
+        </div>
       </div>
     </div>
   );
